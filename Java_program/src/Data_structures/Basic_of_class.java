91e3e84dcfec4a3b404b9ecd8f0f61f1d2e4bf92
gurinder-singh <gurinderjeet30@gmail.com>
2026-03-23 12:52:22 +0530

    Updated some basics

--- Java_program/src/Data_structures/Basic_of_class.java
+++ Java_program/src/Data_structures/Basic_of_class.java
@@ -2,9 +2,33 @@ package Data_structures;
 
 public class Basic_of_class {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+	/*  An object is any entity that has a state and behavior. For example, a bicycle is an object. It has
 
+	States: idle, first gear, etc
+	Behaviors: braking, accelerating, etc.
+	fields are used to store data
+	methods are used to perform some operations
+	*
+	* // state or field
+  		private int gear = 5;
+
+  // behavior or method
+  		public void braking() {
+    System.out.println("Working of Braking");
+	
+	 Creating a new Object 
+	className object = new className();
+
+	for Bicycle class
+	Bicycle sportsBicycle = new Bicycle();
+
+	Bicycle touringBicycle = new Bicycle();
+	
+	*/
+	
+	
+	  
 	}
 
-}
+	
